5f497e5fffa16ceb982ea170f334422f7cf7cce5
Phillip Cloud <cloud@standard.ai>
2021-03-13 17:20:11 -0500

    Clean up nixpkgs-mozilla usage

--- default.nix
+++ default.nix
@@ -1,29 +1,32 @@
 let
   sources = import ./nix/sources.nix;
-
-  rustChannelsOverlay = import "${sources.nixpkgs-mozilla}/rust-overlay.nix";
-  rustChannelsSrcOverlay = import "${sources.nixpkgs-mozilla}/rust-src-overlay.nix";
+  nixpkgs-mozilla = import sources.nixpkgs-mozilla;
 
   pkgs = import sources.nixpkgs {
     overlays = [
-      rustChannelsOverlay
-      rustChannelsSrcOverlay
-      (self: super: {
-        rustc = (self.rustChannelOf {
-          rustToolchain = ./rust-toolchain;
-          sha256 = "19va5fnpbqkllw35rc19q2mixrx9p3m3q5dyi0881c8rcsja7rxc";
-        }).rust.override {
-          extensions = [
-            "clippy-preview"
-            "rls-preview"
-            "rustfmt-preview"
-            "rust-analysis"
-            "rust-analyzer-preview"
-            "rust-std"
-            "rust-src"
-          ];
-        };
-      })
+      nixpkgs-mozilla
+      (self: super:
+        let
+          channel = self.rustChannelOf {
+            rustToolchain = ./rust-toolchain;
+            sha256 = "19va5fnpbqkllw35rc19q2mixrx9p3m3q5dyi0881c8rcsja7rxc";
+          };
+          rust = channel.rust.override {
+            extensions = [
+              "clippy-preview"
+              "rls-preview"
+              "rustfmt-preview"
+              "rust-analysis"
+              "rust-analyzer-preview"
+              "rust-std"
+              "rust-src"
+            ];
+          };
+        in
+        {
+          rustc = rust;
+          cargo = rust;
+        })
     ];
   };
 
