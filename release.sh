c7e220d170c61435e9e50cdd5ee7064cabb7f790
Phillip Cloud <cloud@standard.ai>
2021-03-28 13:01:51 -0400

    Remove EDITOR envar and pass args again

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --keep EDITOR --keep GITHUB_USER --keep GITHUB_TOKEN --pure -i bash -p cacert cargo cargo-release gh git jq yj gh
+#!nix-shell --keep GITHUB_USER --keep GITHUB_TOKEN --pure -i bash -p cacert cargo cargo-release gh git jq yj gh
 
 set -euxo pipefail
 
 cargo release patch
 
 tag="$(yj -tj < Cargo.toml | jq '.package.version' -rcM)"
-gh release create "$tag" --target "$(git rev-parse HEAD)" -t "Release $tag"
+gh release create "$tag" --target "$(git rev-parse HEAD)" -t "Release $tag" "$@"
