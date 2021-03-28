714add6f60c6157f175e6cf6bbd01a408c0e7388
Phillip Cloud <cloud@standard.ai>
2021-03-28 12:51:10 -0400

    Make script pure

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash shell.nix
+#!nix-shell --pure -i bash shell.nix
 
 set -euxo pipefail
 
 cargo release patch
 
 TAG="$(yj -tj < Cargo.toml | jq '.package.version' -rcM)"
-echo gh release create "$TAG" -t "Release $TAG" "$@"
+gh release create "$TAG" -t "Release $TAG" "$@"
